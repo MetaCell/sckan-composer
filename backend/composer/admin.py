48de01bc218dd78f7f0fe0127f18a89dbf56e6fc
Zoran Sinnema <zsinnema@gmail.com>
2022-12-29 16:55:13 +0100

    #13 feat: implement initial database schema and state machine

--- backend/composer/admin.py
+++ backend/composer/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 
-# Register your models here.
+from fsm_admin.mixins import FSMTransitionMixin
+
+from composer.models import AnsDivision, ConnectivityStatement, Profile, Provenance, Specie
+
+
+# Define an inline admin descriptor for Profile model
+# which acts a bit like a singleton
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'profile'
+
+
+# Define a new User admin
+class UserAdmin(BaseUserAdmin):
+    inlines = (ProfileInline,)
+
+
+class ConnectivityStatementAdmin(FSMTransitionMixin, admin.ModelAdmin):
+    # The name of one or more FSMFields on the model to transition
+    fsm_field = ["state",]
+    readonly_fields = ["state",]
+    list_display = ["provenance", "knowledge_statement", "state"]
+
+
+# Re-register UserAdmin
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+
+admin.site.register(AnsDivision)
+admin.site.register(ConnectivityStatement, ConnectivityStatementAdmin)
+admin.site.register(Provenance)
+admin.site.register(Specie)
