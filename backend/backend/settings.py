8cbfb4129f04fdab4c5af47275f012ac23cfffa2
Zoran Sinnema <zsinnema@gmail.com>
2023-01-12 15:35:55 +0100

    fix: for ssl

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -377,4 +377,4 @@ if PRODUCTION:
     # we are behind a rev proxy, so we need to set this
     USE_X_FORWARDED_HOST = True
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-    #SECURE_SSL_REDIRECT = True
+    SECURE_SSL_REDIRECT = True
