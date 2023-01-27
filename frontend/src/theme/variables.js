813c9a94d0fb03d8142c9593e6bd7eaeadadebee
Anto <mrap1988@gmail.com>
2023-01-27 09:53:34 +0100

    FE-main-components: Main layout, topbar, sidebar
    chore: update logo
    
    FE-main-comp feat: Add Topbar
    
    FE-main-components fix: app main layout
    
    FE-main-components feat: add sidebar content and styling

--- frontend/src/theme/variables.js
+++ frontend/src/theme/variables.js
@@ -5,6 +5,11 @@ export const vars = {
     bodyBgColor: '#F2F4F7',
     borderRadius: 8,
     titleFontColor: '#101828',
-    paperShadow:'0px 0px 1px rgba(0, 0, 0, 0.3), 0px 20px 24px -4px rgba(16, 24, 40, 0.08), 0px 8px 8px -4px rgba(16, 24, 40, 0.03)'
+    paperShadow:'0px 0px 1px rgba(0, 0, 0, 0.3), 0px 20px 24px -4px rgba(16, 24, 40, 0.08), 0px 8px 8px -4px rgba(16, 24, 40, 0.03)',
+    grey400:'#98A2B3',
+    topbarShadow:'0px 1px 5px rgba(41, 85, 115, 0.2);',
+    drawerWidth:228,
+    sidebarBg:'#222739',
+    menuItemSelectedBg:'#475467'
   };
   
