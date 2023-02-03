63521b0b05221a1cf49526a243c90f0c0fec9da2
afonso <afonsobspinto@gmail.com>
2023-02-03 11:16:58 +0000

    chore: Remove deprecated utilities

--- frontend/src/utilities/functions.ts
+++
@@ -1,7 +0,0 @@
-export function isEmpty(obj: any) {
-    return Object.keys(obj).length === 0;
-}
-
-export const delay = (delayInMS: number) => {
-    return new Promise(resolve => setTimeout(resolve, delayInMS));
-}
