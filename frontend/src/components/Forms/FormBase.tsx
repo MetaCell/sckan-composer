30ff1351047abd9d4593a4006958d089433dd4ac
Afonso Pinto <afonsobspinto@gmail.com>
2023-02-03 11:42:39 +0000

    Feature/sckan 50 (#69)
    * #50 feat: WIP - Add autosave hook
    * #50 feat: WIP - Update queue system
    * #50 feat: Delete unnecessary hook
    * #50 feat: Update useAutoSave
    * #50 feat: Simplify autosave
    * #50 feat: Block form while saving
    * #50 feat: Replace debug values with prod
    * SCKAN-50 chore: Remove deprecated dependency, deprecated schemas and schema utilities
    * SCKAN-50 chore: some little tweaks on autosaving
    
    ---------
    
    Co-authored-by: Anto <“mrap1988@gmail.com”>
    Co-authored-by: Zoran Sinnema <zsinnema@gmail.com>

--- frontend/src/components/Forms/FormBase.tsx
+++ frontend/src/components/Forms/FormBase.tsx
@@ -1,58 +1,88 @@
-import React from 'react'
+import React, {useRef, useState} from 'react'
 import validator from "@rjsf/validator-ajv8";
-import { IChangeEvent, withTheme } from "@rjsf/core";
-import { Box } from '@mui/material';
-import { Theme } from '@rjsf/mui'
+import {IChangeEvent, withTheme} from "@rjsf/core";
+import {Backdrop, Box, CircularProgress} from '@mui/material';
+import {Theme} from '@rjsf/mui'
+import {useDebouncedCallback} from "use-debounce";
+import {INPUT_DEFAULT_DELAY} from "../../settings";
 
 const Form = withTheme(Theme)
 
 const log = (type: string) => console.log.bind(console, type)
 
+
 export const FormBase = (props: any) => {
-  const { service, data, schema, setter, disabled=false, extraData, uiSchema, uiFields } = props
 
-  if (!data) {
-    return <div>Loading...</div>
-  }
+    const {service, data, schema, setter, extraData, uiSchema, uiFields, enableAutoSave, disabled=false} = props
+    const [localData, setLocalData] = useState<any>(data)
+    const [isSaving, setIsSaving] = useState<boolean>(false)
+    const triggerAutoSave = useDebouncedCallback(() => onSave(), INPUT_DEFAULT_DELAY);
 
-  if (uiFields) {
-    Object.entries(uiSchema).forEach((p) =>
-      !p[0].startsWith("ui:") && !uiFields.includes(p[0])
-        ? delete uiSchema[p[0]] && delete schema.properties[p[0]]
-        : null
+
+    const formRef = useRef<any>(null);
+
+    if (uiFields) {
+        Object.entries(uiSchema).forEach((p) =>
+            !p[0].startsWith("ui:") && !uiFields.includes(p[0])
+                ? delete uiSchema[p[0]] && delete schema.properties[p[0]]
+                : null
+        )
+    }
+
+    const onError = (errors: any) => {
+        log("errors")
+        log(errors)
+    }
+
+    const onSave = () => {
+        if (formRef.current != null) {
+            return formRef.current.submit()
+        }
+    }
+
+    const handleSubmit = async (event: IChangeEvent) => {
+        const formData = {...event.formData, ...extraData}
+        setIsSaving(true)
+        setLocalData(formData)
+        service.save(formData).then((newData: any) => {
+            setter(newData)
+            // todo: Add UI feedback
+            log("Saved")
+        }).catch((error: any) => {
+            // todo: handle errors here
+            log("Something went wrong")
+        }).finally(() => {
+            setIsSaving(false)
+        })
+    }
+
+    const handleUpdate = async (event: IChangeEvent) => {
+        if (enableAutoSave) {
+            return triggerAutoSave()
+        }
+    }
+
+    return (
+      <>
+        {!data || isSaving && <Backdrop
+              open={isSaving}
+          >
+            <CircularProgress color="inherit"/>
+        </Backdrop>
+        }
+        <Box p={2}>
+            <Form
+                ref={formRef}
+                schema={schema}
+                uiSchema={uiSchema}
+                formData={localData}
+                disabled={disabled}
+                validator={validator}
+                onChange={handleUpdate}
+                onSubmit={handleSubmit}
+                onError={onError}
+            />
+        </Box>
+      </>
     )
-  }
-
-  const handleSubmit = (event: IChangeEvent) => {
-    log("submitted")
-    const formData = {...event.formData, ...extraData}
-    service.save(formData).then((newData:any) => {
-      setter(newData)
-    })
-  }
-
-
- const handleUpdate = (event: IChangeEvent) => {
-    log("update")
-  }
-
-  const onError = (errors: any) => {
-    log("errors")
-    log(errors)
-  }
-
-  return (
-    <Box p={2}>
-      <Form
-        schema={schema}
-        uiSchema={uiSchema}
-        formData={data}
-        disabled={disabled}
-        validator={validator}
-        onSubmit={handleSubmit}
-        onError={onError}
-        onChange={handleUpdate}
-      />
-    </Box>
-  )
 }
---
+++ frontend/src/settings.ts
@@ -0,0 +1,1 @@
+export const INPUT_DEFAULT_DELAY = 1500 // 1.5 seconds
