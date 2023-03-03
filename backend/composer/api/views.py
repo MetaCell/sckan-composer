d9575d3e6497eb5788cdc76dd7764cf564f1f712
Zoran Sinnema <zsinnema@gmail.com>
2023-03-03 10:30:22 +0100

    SCKAN-68 chore: remove specie mixin from sentence

--- backend/composer/api/views.py
+++ backend/composer/api/views.py
@@ -237,7 +237,7 @@ class TagViewSet(viewsets.ReadOnlyModelViewSet):
     ]
 
 
-class SentenceViewSet(SpecieMixin, TagMixin, TransitionMixin, AssignOwnerMixin, ModelNoDeleteViewSet):
+class SentenceViewSet(TagMixin, TransitionMixin, AssignOwnerMixin, ModelNoDeleteViewSet):
     """
     Sentence
     """
