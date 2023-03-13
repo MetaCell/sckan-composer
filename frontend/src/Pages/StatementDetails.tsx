0af668b858a36fc261dc3dcf90bc3ab21b502744
Anto <mrap1988@gmail.com>
2023-03-13 13:23:50 +0100

    SCKAN-20 feat: add tabs at statement details page

--- frontend/src/Pages/StatementDetails.tsx
+++ frontend/src/Pages/StatementDetails.tsx
@@ -4,6 +4,9 @@ import Paper from "@mui/material/Paper";
 import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid";
+import Tab from "@mui/material/Tab";
+import Tabs from "@mui/material/Tabs";
+import TabPanel from "../components/Widgets/TabPanel";
 import { useParams } from "react-router-dom";
 import StatementForm from "../components/Forms/StatementForm";
 import statementService from "../services/StatementService";
@@ -19,6 +22,7 @@ const StatementDetails = () => {
   const { statementId } = useParams();
   const [statement, setStatement] = useState({} as ConnectivityStatement);
   const [loading, setLoading] = useState(true);
+  const [activeTab, setActiveTab] = useState(0);
 
   const doTransition = (transition: string) => {
     statementService
@@ -82,8 +86,6 @@ const StatementDetails = () => {
             </Box>
           </Stack>
         </Paper>
-      </Grid>
-      <Grid item xl={7}>
         <Typography>
           Last modified by {statement?.owner?.first_name} on{" "}
           {statement?.modified_date}
@@ -92,20 +94,36 @@ const StatementDetails = () => {
           <Button onClick={() => doTransition(transition)}>{transition}</Button>
         ))}
         <CheckDuplicates />
-        <StatementForm data={statement} format="full" setter={setStatement} />
       </Grid>
-      <Grid item xl={5}>
-        <TagForm
-          data={statement.tags}
-          extraData={{ parentId: statement.id, service: statementService }}
-          setter={setStatement}
-        />
+      <Grid item xl={7}>
+        <Box>
+          <Tabs
+            value={activeTab}
+            variant="fullWidth"
+            onChange={(e, i: number) => setActiveTab(i)}
+          >
+            <Tab label="Distillation" />
+            <Tab label="Proofing" />
+          </Tabs>
+          <TabPanel value={activeTab} index={0}>
+            <StatementForm
+              data={statement}
+              format="full"
+              setter={setStatement}
+            />
+            <TagForm
+              data={statement.tags}
+              extraData={{ parentId: statement.id, service: statementService }}
+              setter={setStatement}
+            />
+          </TabPanel>
+          <TabPanel value={activeTab} index={1}>
+            <ProofingTab statement={statement} />
+          </TabPanel>
+        </Box>
       </Grid>
       <Grid item xl={5}>
         <NoteForm extraData={{ connectivity_statement_id: statement.id }} />
-      </Grid>
-      <Grid item xl={5}>
-        <ProofingTab statement={statement} />
       </Grid>
     </Grid>
   );
