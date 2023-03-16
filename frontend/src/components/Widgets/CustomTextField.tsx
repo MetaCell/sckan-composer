f13ddaa451990186bc47a6daf4b931f6e71be0e1
Zoran Sinnema <zsinnema@gmail.com>
2023-03-16 14:59:28 +0100

    SCKAN-16 fix: add value to custom text input so the current value will be shown

--- frontend/src/components/Widgets/CustomTextField.tsx
+++ frontend/src/components/Widgets/CustomTextField.tsx
@@ -15,6 +15,7 @@ const StyledInput = styled(TextField)(({ theme }) => ({
 }));
 
 export default function CustomTextField({
+  value,
   placeholder,
   onChange,
   options: { label, multiline, rows },
@@ -32,6 +33,7 @@ export default function CustomTextField({
         placeholder={placeholder}
         multiline={multiline}
         rows={rows}
+        value={value}
       />
     </FormControl>
   );
