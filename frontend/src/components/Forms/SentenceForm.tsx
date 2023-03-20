34d24e103d69323c1ac851df22e861100d45a912
Zoran Sinnema <zsinnema@gmail.com>
2023-03-20 09:03:21 +0100

    SCKAN-16 feat: add pmcid and doi deeplinks of sentence

--- frontend/src/components/Forms/SentenceForm.tsx
+++ frontend/src/components/Forms/SentenceForm.tsx
@@ -12,6 +12,36 @@ import Chip from "@mui/material/Chip";
 import OpenInNewIcon from '@mui/icons-material/OpenInNew';
 import {useNavigate} from "react-router";
 
+const linkedChip = (data: any) => {
+  if (data.id === undefined || data.id === null) {
+    return null
+  }
+
+  const handleOpenId = (e: any) => {
+    window.open(data?.uri, '_blank')
+  }
+
+  return (
+    <Chip
+      label={`${data.label}:${data.id}`}
+      variant="filled"
+      deleteIcon={<OpenInNewIcon />}
+      onDelete={() => handleOpenId(data.uri)}
+      sx={{
+        background: vars.lightBlue,
+        color: vars.darkBlue,
+        marginLeft: '10px',
+        marginRight: '10px',
+
+        "& .MuiChip-deleteIcon": {
+          fontSize: '14px',
+          color: vars.mediumBlue
+        }
+      }}
+    />
+  )
+}
+
 const SentenceForm = (props: any) => {
   const { format, data } = props
   const { schema, uiSchema } = jsonSchemas.getSentenceSchema()
@@ -67,25 +97,17 @@ const SentenceForm = (props: any) => {
         {...props}
       />
       <Box mt={2}>
-        <Typography color={vars.labelColor} fontWeight={500} mb={1}>
+        {/* <Typography color={vars.labelColor} fontWeight={500} mb={1}>
           PMID (PubMed identifier)
-        </Typography>
-        {
-          data?.pmid && <Chip
-            label={data?.pmid}
-            variant="filled"
-            deleteIcon={<OpenInNewIcon />}
-            onDelete={handleOpenPmid}
-            sx={{
-              background: vars.lightBlue,
-              color: vars.darkBlue,
-
-              "& .MuiChip-deleteIcon": {
-                fontSize: '14px',
-                color: vars.mediumBlue
-              }
-            }}
-          />
+        </Typography> */}
+        { 
+          linkedChip({id: data?.pmid, uri: data?.pmid_uri, label: "PMID"})
+        }
+        { 
+          linkedChip({id: data?.pmcid, uri: data?.pmcid_uri, label: "PMCID"})
+        }
+        { 
+          linkedChip({id: data?.doi, uri: data?.doi_uri, label: "DOI"})
         }
       </Box>
     </Paper>
