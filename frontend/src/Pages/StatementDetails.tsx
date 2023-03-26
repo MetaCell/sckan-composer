b72c061758b168bae6bf14b98f4453666e6e4949
Salam Dalloul <salam@leannode.com>
2023-03-26 03:47:41 +0300

    view notes/tags form + statement header

--- frontend/src/Pages/StatementDetails.tsx
+++ frontend/src/Pages/StatementDetails.tsx
@@ -17,6 +17,15 @@ import { Button } from "@mui/material";
 import { userProfile } from "../services/UserService";
 import CheckDuplicates from "../components/CheckForDuplicates/CheckDuplicatesDialog";
 import ProofingTab from "../components/ProofingTab";
+import {SentenceStateChip} from "../components/Widgets/StateChip";
+import {formatDate, formatTime, SentenceLabels} from "../helpers/helpers";
+import GroupedButtons from "../components/Widgets/CustomGroupedButtons";
+import TriageStatementSection from "../components/TriageStatementSection/TriageStatementSection";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+import SentenceForm from "../components/Forms/SentenceForm";
+import sentenceService from "../services/SentenceService";
+import Divider from "@mui/material/Divider";
+import NoteDetails from "../components/Widgets/NotesFomList";
 
 const StatementDetails = () => {
   const { statementId } = useParams();
@@ -66,66 +75,104 @@ const StatementDetails = () => {
   }
 
   return (
-    <Grid p={12} container justifyContent="space-between">
-      <Grid item xl={12}>
-        <Paper elevation={0} sx={{ padding: 8 }}>
-          <Stack alignItems="center" spacing={4}>
-            <Box textAlign="center">
-              <Typography variant="h3" marginBottom={1.5}>
-                Statement Details
+    <Grid p={6} container>
+      <Grid item xs={12} mb={4}>
+        <Grid container>
+          <Grid item xs={12} md={6}>
+            <Box>
+              <Typography variant="h3" mb={1}>
+                Statement Details #{statementId}{" "}
+                <span>
+                  <SentenceStateChip
+                    key={statement?.state}
+                    value={statement?.state}
+                  />
+                </span>
               </Typography>
-              <Typography variant="subtitle2">
-                Show the statement with id {statementId} details here
-              </Typography>
-              <Typography variant="subtitle2">
-                Sentence: {statement?.sentence.title}
-              </Typography>
-              <Typography variant="subtitle2">
-                {statement?.sentence.text}
-              </Typography>
+              <span>
+                Last Edited on {formatDate(statement?.modified_date)},{" "}
+                {formatTime(statement?.modified_date)}
+              </span>
             </Box>
-          </Stack>
-        </Paper>
-        <Typography>
-          Last modified by {statement?.owner?.first_name} on{" "}
-          {statement?.modified_date}
-        </Typography>
-        {statement?.available_transitions.map((transition) => (
-          <Button key={transition} onClick={() => doTransition(transition)}>
-            {transition}
-          </Button>
-        ))}
+          </Grid>
+          <Grid item xs={12} md={6} display="flex" justifyContent="flex-end">
+            <Button>status</Button>
+          </Grid>
+        </Grid>
       </Grid>
-      <Grid item xl={7}>
+
+      <Grid item xl={12} mb={4} width={1}>
         <Box>
           <Tabs
             value={activeTab}
             variant="standard"
             onChange={(e, i: number) => setActiveTab(i)}
+            sx={{
+              borderBottom: '1px solid #EAECF0',
+            }}
           >
             <Tab label="Distillation" />
             <Tab label="Proofing" />
           </Tabs>
-          <TabPanel value={activeTab} index={0}>
-            <CheckDuplicates />
-            <StatementForm
-              data={statement}
-              format="full"
-              setter={setStatement}
-            />
-            <TagForm
-              data={statement.tags}
-              extraData={{ parentId: statement.id, service: statementService }}
-              setter={setStatement}
-            />
-          </TabPanel>
-          <TabPanel value={activeTab} index={1}>
-            <ProofingTab statement={statement} />
-          </TabPanel>
         </Box>
       </Grid>
-      <Grid item xl={5}>
-        <NoteForm extraData={{ connectivity_statement_id: statement.id }} />
+
+      <Grid item xs={12}>
+        <Grid container spacing={1}>
+          <Grid item xs={12} md={7}>
+            <TabPanel value={activeTab} index={0}>
+              <Paper
+                sx={{
+                  border: 0,
+                  boxShadow: "none",
+                }}
+              >
+                <Grid container p={3} mb={2}>
+                  <Grid item xs={12}>
+                    <Stack
+                      direction="row"
+                      justifyContent="space-between"
+                      sx={{
+                        "& .MuiButtonBase-root": {
+                          padding: 0,
+                        },
+                      }}
+                    >
+                      <Typography variant="h5" mb={1}>
+                        Knowledge Statements
+                      </Typography>
+                    </Stack>
+                  </Grid>
+                </Grid>
+              </Paper>
+              <SentenceForm
+                data={statement.sentence}
+                format="small"
+              />
+            </TabPanel>
+            <TabPanel value={activeTab} index={1}>
+              <ProofingTab statement={statement} />
+            </TabPanel>
+
+          </Grid>
+
+          <Grid item xs={12} md={5} p={1}>
+            <Paper sx={{ padding: "24px", "& .MuiBox-root": { padding: 0 } }}>
+              <Box>
+                <Typography variant="h5" mb={1}>
+                  Notes
+                </Typography>
+              </Box>
+              <TagForm
+                data={statement.tags}
+                extraData={{ parentId: statement.id, service: statementService }}
+                setter={setStatement}
+              />
+              <Divider sx={{ margin: "36px 0" }} />
+              <NoteDetails extraData={{ sentence_id: statement.id }} />
+            </Paper>
+          </Grid>
+        </Grid>
       </Grid>
     </Grid>
   );
