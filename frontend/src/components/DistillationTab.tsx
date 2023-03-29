f4ba5665803479152e06a30762d30f4a3aa52aad
Salam Dalloul <salam@leannode.com>
2023-03-29 02:03:42 +0200

    SCKAN-93 view related NLP statements

--- frontend/src/components/DistillationTab.tsx
+++ frontend/src/components/DistillationTab.tsx
@@ -7,6 +7,10 @@ import SentenceForm from "../components/Forms/SentenceForm";
 import SpeciesForm from "../components/Forms/SpeciesForm";
 import StatementForm from "../components/Forms/StatementForm";
 import Paper from "@mui/material/Paper";
+import OpenInNewIcon from '@mui/icons-material/OpenInNew';
+import {Box} from "@mui/material";
+import Divider from "@mui/material/Divider";
+import Chip from "@mui/material/Chip";
 
 const DistillationTab = ({ statement, setStatement } : any) => {
 
@@ -14,9 +18,72 @@ const DistillationTab = ({ statement, setStatement } : any) => {
     <Grid container mb={2} spacing={2}>
       <Grid item xs={12}>
         <Paper>
-          <Typography variant="h5" mb={1}>
+          <Typography variant="h5" mb={3}>
             Knowledge Statements
           </Typography>
+          <Box sx={{
+            background: '#F2F4F7',
+            borderRadius: '12px',
+            padding: '8px !important',
+            textAlign: 'center',
+
+            "& .MuiBox-root": {
+              background: '#fff',
+              borderRadius: '12px',
+              border: '1px solid #EAECF0',
+              textAlign: 'left'
+            },
+
+            "& .MuiTypography-root": {
+              padding: '12px'
+            }
+          }}>
+              <Box>
+                <Typography>Superior cervical ganglion to sphincter papillae via internal carotid nerve plexus via ophthalmic plexus via long ciliary nerve</Typography>
+                <Divider sx={{borderColor: '#EAECF0'}} />
+                <Typography>
+                  <Chip
+                    variant="outlined"
+                    label='https://loremipsum.com/paper'
+                    sx={{borderRadius: '6px', border: '1px solid #D0D5DD'}}
+                  />
+                </Typography>
+              </Box>
+          </Box>
+          <Divider sx={{margin: '24px 0'}}>Records from the same NLP Sentence</Divider>
+          <Stack
+            direction={{ xs: 'column', sm: 'row' }}
+            spacing={{ xs: 1, sm: 2 }}
+            alignItems='center'
+          >
+            <Box sx={{
+              borderRadius: '12px',
+              border: '1px solid #EAECF0',
+              textAlign: 'left',
+              background: '#F9FAFB',
+
+              "& .MuiTypography-root": {
+                padding: '12px',
+              }
+            }}>
+              <Typography>Superior cervical ganglion to sphincter papillae via internal carotid nerve plexus via ophthalmic plexus via long ciliary nerve</Typography>
+              <Divider sx={{borderColor: '#EAECF0'}} />
+              <Typography>
+                <Chip
+                  variant="outlined"
+                  label='https://loremipsum.com/paper'
+                  sx={{borderRadius: '6px', border: '1px solid #D0D5DD', background: '#fff'}}
+                />
+              </Typography>
+            </Box>
+            <Box>
+              <OpenInNewIcon sx={{
+                color: '#98A2B3',
+                fontSize: '12px'
+              }} />
+            </Box>
+          </Stack>
+
         </Paper>
       </Grid>
 
