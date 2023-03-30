1d4e3fa7604bcfb441d4eab8440f3f600b57ed3c
Zoran Sinnema <zsinnema@gmail.com>
2023-03-30 14:41:28 +0200

    chore: fix small bug with put on CS

--- frontend/src/helpers/helpers.ts
+++ frontend/src/helpers/helpers.ts
@@ -139,8 +139,17 @@ export const timeAgo = (timestamp : string) => {
   const seconds = Math.floor(timeDiff / 1000);
   const minutes = Math.floor(seconds / 60);
   const hours = Math.floor(minutes / 60);
+  const days = Math.floor(hours / 24);
+  const weeks = Math.floor(days / 7);
+  const years = Math.floor(weeks / 52);
 
-  if (hours > 0) {
+  if (years > 0) {
+    return `${years} year${years > 1 ? 's' : ''} ago`;
+  } else if (weeks > 0) {
+    return `${weeks} week${weeks > 1 ? 's' : ''} ago`;
+  } else if (days > 0) {
+    return `${days} day${days > 1 ? 's' : ''} ago`;
+  } else if (hours > 0) {
     return `${hours} hour${hours > 1 ? 's' : ''} ago`;
   } else if (minutes > 0) {
     return `${minutes} minute${minutes > 1 ? 's' : ''} ago`;
