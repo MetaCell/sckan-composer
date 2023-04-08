c95534dca658c9529fb618892e7d6be32ee4328d
Zoran Sinnema <zsinnema@gmail.com>
2023-04-08 19:59:28 +0200

    chore: add composer to nginx proxy to django backend

--- backend/composer/urls.py
+++ backend/composer/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path("api/composer/", include("composer.api.urls")),
-    path("export", views.export, name="export"),
+    path("composer/export", views.export, name="export"),
     path("login", views.index, name="index"),
     path("logged-out/", views.logout_landing, name="logged-out"),
 ]
