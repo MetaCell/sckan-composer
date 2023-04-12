e6ec01ea1e56f58fbc7d0217ded2bfbbf326bf19
Simão sá <simao@metacell.us>
2023-04-12 14:45:14 +0100

    sckan-108 added sample test and jest/puppeteer installations

---
+++ frontend/test/e2e.test.js
@@ -0,0 +1,58 @@
+//IMPORTS:
+const puppeteer = require("puppeteer");
+
+
+
+
+const baseURL = process.env.TEST_URL || 'https://composer.sckan.dev.metacell.us/'
+
+
+
+//TESTS:
+let page;
+let browser;
+jest.setTimeout(60000 * 2);
+
+describe('End to End Tests', () => {
+
+    beforeAll(async () => {
+
+        browser = await puppeteer.launch({
+            args: [
+                "--no-sandbox",
+                `--window-size=1600,1000`,
+                "--ignore-certificate-errors"
+            ],
+            headless: !process.env.PUPPETEER_DISPLAY,
+            defaultViewport: {
+                width: 1600,
+                height: 1000,
+            },
+        });
+
+
+        page = await browser.newPage();
+        console.log(
+            "Checking page",
+            baseURL
+        );
+        console.log('Starting tests ...')
+
+        page.on("pageerror", err => {
+            console.log('ERROR')
+            throw new Error(`Page error: ${err.toString()}`);
+        });
+
+        await page.goto(baseURL);
+    });
+
+    afterAll(() => {
+
+        browser.close()
+    })
+
+    it('HomePage', async () => {
+
+        await page.waitForSelector('#username')
+    })
+})
