6e3a6fded39530d56bd2ff357b5f513557940b75
Afonso Pinto <afonsobspinto@gmail.com>
2023-09-13 13:27:40 +0100

    SCKAN-148 chore: Rollback unintentional code style changes

--- frontend/src/Pages/SentenceDetails.tsx
+++ frontend/src/Pages/SentenceDetails.tsx
@@ -123,9 +123,9 @@ const SentencesDetails = () => {
     ]);
   };
 
-  const handleMenuItemClick = (
-      event: React.MouseEvent<HTMLLIElement, MouseEvent>,
-      index: number
+const handleMenuItemClick = (
+    event: React.MouseEvent<HTMLLIElement, MouseEvent>,
+    index: number
   ) => {
     setSelectedIndex(index);
     setOpen(false);
@@ -138,38 +138,36 @@ const SentencesDetails = () => {
   useEffect(() => {
     if (sentenceId) {
       sentenceService
-          .getObject(sentenceId)
-          .then((sentence: Sentence) => {
-            setSentence(sentence);
-            const foundToBeReviewed = sentence.available_transitions.findIndex((transition) => transition === SentenceAvailableTransitionsEnum.ToBeReviewed)
-            setSelectedIndex(foundToBeReviewed && foundToBeReviewed !== -1 ? foundToBeReviewed : 0)
-            setConnectivityStatements(sentence.connectivity_statements.sort((a,b)=>a.id-b.id));
+        .getObject(sentenceId)
+        .then((sentence: Sentence) => {
+          setSentence(sentence);
+          const foundToBeReviewed = sentence.available_transitions.findIndex((transition) => transition === SentenceAvailableTransitionsEnum.ToBeReviewed)
+          setSelectedIndex(foundToBeReviewed && foundToBeReviewed !== -1 ? foundToBeReviewed : 0)
+          setConnectivityStatements(sentence.connectivity_statements.sort((a,b)=>a.id-b.id));
+          if (
+            sentence.owner &&
+            sentence.owner?.id !== userProfile.getUser().id
+          ) {
             if (
-                sentence.owner &&
-                sentence.owner?.id !== userProfile.getUser().id
+              window.confirm(
+                `This sentence is assigned to ${sentence.owner.first_name}, assign to yourself? To view the record without assigning ownership, select Cancel.`
+              )
             ) {
-              if (
-                  window.confirm(
-                      `This sentence is assigned to ${sentence.owner.first_name}, assign to yourself? To view the record without assigning ownership, select Cancel.`
-                  )
-              ) {
-                sentenceService
-                    .save({ ...sentence, owner_id: userProfile.getUser().id })
-                    .then((sentence: Sentence) => {
-                      setSentence(sentence);
-                    });
-              }
+              sentenceService
+                .save({ ...sentence, owner_id: userProfile.getUser().id })
+                .then((sentence: Sentence) => {
+                  setSentence(sentence);
+                });
             }
-          })
-          .finally(() => {
-            setRefetch(false);
-            setIsLoading(false);
-          });
+          }
+        })
+        .finally(() => {
+          setRefetch(false);
+          setLoading(false);
+        });
     }
   }, [sentenceId, refetch]);
-
-  console.log(isLoading)
-
+  
   if (isLoading) {
     return (
         <Backdrop open={isLoading} sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}>
@@ -181,114 +179,114 @@ const SentencesDetails = () => {
   const disabled = sentence.owner?.id !== userProfile.getUser().id;
 
   return (
-      <Grid p={6} container>
-        <Grid item xs={12} mb={4}>
-          <Grid container>
-            <Grid item xs={12} md={6}>
-              <Box>
-                <Typography variant="h3" mb={1}>
-                  Sentence Details #{sentenceId}{" "}
-                  <span>
+    <Grid p={6} container>
+      <Grid item xs={12} mb={4}>
+        <Grid container>
+          <Grid item xs={12} md={6}>
+            <Box>
+              <Typography variant="h3" mb={1}>
+                Sentence Details #{sentenceId}{" "}
+                <span>
                   <SentenceStateChip
-                      key={sentence?.state}
-                      value={sentence?.state}
+                    key={sentence?.state}
+                    value={sentence?.state}
                   />
                 </span>
-                </Typography>
-                <span>
+              </Typography>
+              <span>
                 Last Edited on {formatDate(sentence?.modified_date)},{" "}
-                  {formatTime(sentence?.modified_date)}
+                {formatTime(sentence?.modified_date)}
               </span>
-              </Box>
-            </Grid>
-            <Grid item xs={12} md={6} display="flex" justifyContent="flex-end">
-              {!disabled && sentence?.available_transitions?.length !== 0 && (
-                  <GroupedButtons
-                      handleClick={handleClick}
-                      selectedOption={
-                        SentenceLabels[sentence?.available_transitions[selectedIndex]]
-                      }
-                      options={sentence?.available_transitions}
-                      selectedIndex={selectedIndex}
-                      handleMenuItemClick={handleMenuItemClick}
-                      hasFormat={true}
-                      format={SentenceLabels}
-                  />
-              )}
-            </Grid>
+            </Box>
           </Grid>
-        </Grid>
-
-        <Grid item xs={12}>
-          <Grid container spacing={2}>
-            <Grid item xs={12} md={7}>
-              <Paper sx={{mb:2, ...sectionStyle}}>
-                <Grid container>
-                  <Grid item xs={12}>
-                    <Stack
-                        direction="row"
-                        justifyContent="space-between"
-                        sx={{
-                          "& .MuiButtonBase-root": {
-                            padding: 0,
-                          },
-                        }}
-                    >
-                      <Typography variant="h5" mb={3}>
-                        Knowledge Statements
-                      </Typography>
-                      <CheckDuplicates />
-                    </Stack>
-                  </Grid>
-                  {connectivityStatements?.map((statement, key) => (
-                      <TriageStatementSection
-                          statement={statement}
-                          key={key}
-                          index={key}
-                          refreshSentence={refreshSentence}
-                          setRefresh={setRefetch}
-                          setSentence={setSentence}
-                          sentence={sentence}
-                      />
-                  ))}
-                  <StyledAddStatementBtn
-                      startIcon={<AddCircleIcon />}
-                      variant="contained"
-                      fullWidth={true}
-                      onClick={onAddNewStatement}
-                  >
-                    Add a knowledge statement
-                  </StyledAddStatementBtn>
-                </Grid>
-              </Paper>
-              <SentenceForm
-                  data={sentence}
-                  disabled={disabled}
-                  format="small"
-                  setter={setSentence}
-                  enableAutoSave={true}
+          <Grid item xs={12} md={6} display="flex" justifyContent="flex-end">
+            {!disabled && sentence?.available_transitions?.length !== 0 && (
+              <GroupedButtons
+                handleClick={handleClick}
+                selectedOption={
+                  SentenceLabels[sentence?.available_transitions[selectedIndex]]
+                }
+                options={sentence?.available_transitions}
+                selectedIndex={selectedIndex}
+                handleMenuItemClick={handleMenuItemClick}
+                hasFormat={true}
+                format={SentenceLabels}
               />
-            </Grid>
-
-            <Grid item xs={12} md={5}>
-              <Paper sx={{...sectionStyle , "& .MuiBox-root": { padding: 0 }}}>
-                <Box>
-                  <Typography variant="h5" mb={1}>
-                    Notes
-                  </Typography>
-                </Box>
-                <TagForm
-                    data={sentence.tags}
-                    extraData={{ parentId: sentence.id, service: sentenceService }}
-                    setter={refreshSentence}
-                />
-                <Divider sx={{ margin: "36px 0" }} />
-                <NoteDetails extraData={{ sentence_id: sentence.id }} />
-              </Paper>
-            </Grid>
+            )}
           </Grid>
         </Grid>
       </Grid>
+
+      <Grid item xs={12}>
+        <Grid container spacing={2}>
+          <Grid item xs={12} md={7}>
+            <Paper sx={{mb:2, ...sectionStyle}}>
+              <Grid container>
+                <Grid item xs={12}>
+                  <Stack
+                    direction="row"
+                    justifyContent="space-between"
+                    sx={{
+                      "& .MuiButtonBase-root": {
+                        padding: 0,
+                      },
+                    }}
+                  >
+                    <Typography variant="h5" mb={3}>
+                      Knowledge Statements
+                    </Typography>
+                    <CheckDuplicates />
+                  </Stack>
+                </Grid>
+                {connectivityStatements?.map((statement, key) => (
+                  <TriageStatementSection
+                    statement={statement}
+                    key={key}
+                    index={key}
+                    refreshSentence={refreshSentence}
+                    setRefresh={setRefetch}
+                    setSentence={setSentence}
+                    sentence={sentence}
+                  />
+                ))}
+                <StyledAddStatementBtn
+                  startIcon={<AddCircleIcon />}
+                  variant="contained"
+                  fullWidth={true}
+                  onClick={onAddNewStatement}
+                >
+                  Add a knowledge statement
+                </StyledAddStatementBtn>
+              </Grid>
+            </Paper>
+            <SentenceForm
+              data={sentence}
+              disabled={disabled}
+              format="small"
+              setter={setSentence}
+              enableAutoSave={true}
+            />
+          </Grid>
+
+          <Grid item xs={12} md={5}>
+            <Paper sx={{...sectionStyle , "& .MuiBox-root": { padding: 0 }}}>
+              <Box>
+                <Typography variant="h5" mb={1}>
+                  Notes
+                </Typography>
+              </Box>
+              <TagForm
+                data={sentence.tags}
+                extraData={{ parentId: sentence.id, service: sentenceService }}
+                setter={refreshSentence}
+              />
+              <Divider sx={{ margin: "36px 0" }} />
+              <NoteDetails extraData={{ sentence_id: sentence.id }} />
+            </Paper>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
   );
 };
 
