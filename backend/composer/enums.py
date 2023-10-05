407848b951551fc2e3626d60b07606ebb1f6ef55
afonso pinto <afonsobspinto@gmail.com>
2023-10-05 01:01:04 +0100

    SCKAN-167 feat: Add forward connections to export

--- backend/composer/enums.py
+++ backend/composer/enums.py
@@ -85,6 +85,7 @@ class ExportRelationships(models.TextChoices):
     hasAxonSensorySubcellularElementIn = "hasAxonSensorySubcellularElementIn", "Afferent terminal",
     hasAxonLocatedIn = "hasAxonLocatedIn", "Axon"
     hasDendriteLocatedIn = "hasDendriteLocatedIn", "Dendrite"
+    hasForwardConnection = "hasForwardConnection", "Forward Connection"
 
 
 class MetricEntity(models.TextChoices):
