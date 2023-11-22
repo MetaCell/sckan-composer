cf47d4a35a6450c27b4fd883c19c58108cab0739
afonso pinto <afonsobspinto@gmail.com>
2023-11-22 23:30:22 +0000

    SCKAN-173 feat: Update journey compilation algorithm

--- backend/composer/services/graph_service.py
+++ backend/composer/services/graph_service.py
@@ -1,51 +1,65 @@
 def generate_paths(origins, vias, destinations):
     paths = []
+    # Calculate the total number of layers, including origins and destinations
+    number_of_layers = len(vias) + 2 if vias else 2
 
-    # If there are no vias, handle direct connections from origins to destinations
-    if not vias:
+    # Handle direct connections from origins to destinations
+    for origin in origins:
+        for destination in destinations:
+            # Check if there's a direct connection between the origin and the destination
+            if origin in destination.from_entities.all() or not destination.from_entities.exists():
+                for dest_entity in destination.anatomical_entities.all():
+                    # Add path with origin and destination at their respective layers
+                    paths.append([(origin.name, 0), (dest_entity.name, number_of_layers - 1)])
+
+    # Handle connections involving vias
+    if vias:
         for origin in origins:
-            for destination in destinations:
-                if origin in destination.from_entities.all() or not destination.from_entities.exists():
-                    # Assuming destination.anatomical_entities.all() returns AnatomicalEntity objects
-                    for dest_entity in destination.anatomical_entities.all():
-                        paths.append([origin.name, dest_entity.name])
-    else:
-        # Handle connections involving vias
-        for origin in origins:
-            paths.extend(create_paths_from_origin(origin, vias, destinations, [origin.name]))
+            # Generate paths through vias for each origin
+            paths.extend(create_paths_from_origin(origin, vias, destinations, [(origin.name, 0)], number_of_layers))
 
-    return paths
+    # Remove duplicates from the generated paths
+    unique_paths = [list(path) for path in set(tuple(path) for path in paths)]
+
+    return unique_paths
 
 
-def create_paths_from_origin(origin, vias, destinations, current_path):
+def create_paths_from_origin(origin, vias, destinations, current_path, number_of_layers):
+    # Base case: if there are no more vias to process
     if not vias:
-        # If there are no more vias, connect directly to destinations
-        return [current_path + [dest_entity.name] for dest in destinations for dest_entity in
-                dest.anatomical_entities.all()
-                if
-                current_path[-1] in list(a.name for a in dest.from_entities.all()) or not dest.from_entities.exists()]
-
-    # Current via layer to process
-    current_via = vias[0]
+        # Generate direct connections from the current path to destinations
+        return [current_path + [(dest_entity.name, number_of_layers - 1)] for dest in destinations
+                for dest_entity in dest.anatomical_entities.all()
+                if current_path[-1][0] in list(
+                a.name for a in dest.from_entities.all()) or not dest.from_entities.exists()]
 
     new_paths = []
-    # Check direct connections to destinations before proceeding to next via
-    for dest in destinations:
-        for dest_entity in dest.anatomical_entities.all():
-            if current_path[-1] in list(a.name for a in dest.from_entities.all()):
-                new_paths.append(current_path + [dest_entity.name])
-
-    for entity in current_via.anatomical_entities.all():
-        if current_path[-1] in list(
+    for idx, current_via in enumerate(vias):
+        # Determine the layer of the current via
+        via_layer = current_via.order + 1
+        # This checks if the last node in the current path is one of the nodes that can lead to the current via.
+        # In other words, it checks if there is a valid connection
+        # from the last node in the current path to the current via.
+        if current_path[-1][0] in list(
                 a.name for a in current_via.from_entities.all()) or not current_via.from_entities.exists():
-            # Proceed to next layer or to destinations
-            new_paths.extend(create_paths_from_origin(origin, vias[1:], destinations, current_path + [entity.name]))
+            for entity in current_via.anatomical_entities.all():
+                # Build new sub-paths including the current via entity
+                new_sub_path = current_path + [(entity.name, via_layer)]
+                # Recursively call to build paths from the next vias
+                new_paths.extend(
+                    create_paths_from_origin(origin, vias[idx + 1:], destinations, new_sub_path, number_of_layers))
+
+                # Check for direct connections to destinations from the current via
+                for dest in destinations:
+                    for dest_entity in dest.anatomical_entities.all():
+                        if entity.name in list(a.name for a in dest.from_entities.all()):
+                            # Add path to destinations directly from the current via
+                            new_paths.append(new_sub_path + [(dest_entity.name, number_of_layers - 1)])
 
     return new_paths
 
 
 def consolidate_paths(paths):
-    # Flag to track if any merges happened in the current iteration
     merges_made = True
 
     while merges_made:
@@ -57,42 +71,54 @@ def consolidate_paths(paths):
             if i in used_indices:
                 continue
 
+            merged_path = path
             for j, other_path in enumerate(paths):
                 if i != j and j not in used_indices and can_merge(path, other_path):
-                    # Merge paths and mark indices as used
-                    path = merge_paths(path, other_path)
+                    merged_path = merge_paths(merged_path, other_path)
                     used_indices.add(j)
                     merges_made = True
 
-            consolidated.append(path)
+            consolidated.append(merged_path)
 
-        paths = consolidated
+        paths = [p for i, p in enumerate(consolidated) if i not in used_indices]
 
-    return consolidated
+    return paths
 
 
 def can_merge(path1, path2):
+    # Ensure paths are of the same length
     if len(path1) != len(path2):
         return False
 
-    difference_count = 0
-    for node1, node2 in zip(path1, path2):
-        if node1 != node2:
-            difference_count += 1
-            if difference_count > 1:
+    differences = 0
+    for (p1, layer1), (p2, layer2) in zip(path1, path2):
+        # Split nodes into individual entities
+        p1_entities = set(p1.split(", "))
+        p2_entities = set(p2.split(", "))
+
+        # Only consider merging if the layers are the same
+        if layer1 == layer2:
+            # Check if the entities differ and are not subsets of each other
+            if p1_entities != p2_entities and not p1_entities.issubset(p2_entities) and not p2_entities.issubset(
+                    p1_entities):
+                differences += 1
+                if differences > 1:
+                    return False
+        else:
+            # If layers are different and entities are not identical, it's a difference
+            if p1_entities != p2_entities:
                 return False
 
-    return difference_count == 1
+    # Only allow merging if there is exactly one difference and it occurs in the same layer
+    return differences == 1
 
 
 def merge_paths(path1, path2):
     merged_path = []
-    for node1, node2 in zip(path1, path2):
-        if node1 == node2:
-            merged_path.append(node1)
+    for (p1, layer1), (p2, layer2) in zip(path1, path2):
+        if p1 == p2 and layer1 == layer2:
+            merged_path.append((p1, layer1))
         else:
-            # Combine different nodes and ensure no duplicates
-            merged_nodes = set(node1.split(", ") + node2.split(", "))
-            merged_path.append(", ".join(sorted(merged_nodes)))
-
+            merged_nodes = set(p1.split(", ") + p2.split(", "))
+            merged_path.append((", ".join(sorted(merged_nodes)), layer1))
     return merged_path
--- backend/tests/test_journey.py
+++ backend/tests/test_journey.py
@@ -41,8 +41,8 @@ class JourneyTestCase(TestCase):
         destinations = list(Destination.objects.filter(connectivity_statement=cs))
 
         expected_paths = [
-            ['Oa', 'V1a', 'Da'],
-            ['Ob', 'Da']
+            [('Oa', 0), ('V1a', 1), ('Da', 2)],
+            [('Ob', 0), ('Da', 2)]
         ]
 
         all_paths = generate_paths(origins, vias, destinations)
@@ -52,9 +52,10 @@ class JourneyTestCase(TestCase):
         self.assertTrue(all_paths == expected_paths)
 
         expected_journey = [
-            ['Oa', 'V1a', 'Da'],
-            ['Ob', 'Da']
+            [('Oa', 0), ('V1a', 1), ('Da', 2)],
+            [('Ob', 0), ('Da', 2)]
         ]
+
         journey_paths = consolidate_paths(all_paths)
         expected_journey.sort()
         journey_paths.sort()
@@ -88,8 +89,8 @@ class JourneyTestCase(TestCase):
         destinations = list(Destination.objects.filter(connectivity_statement=cs))
 
         expected_paths = [
-            ['Oa', 'Da'],
-            ['Ob', 'Da'],
+            [('Oa', 0), ('Da', 1)],
+            [('Ob', 0), ('Da', 1)]
         ]
 
         all_paths = generate_paths(origins, None, destinations)
@@ -99,9 +100,7 @@ class JourneyTestCase(TestCase):
         self.assertTrue(all_paths == expected_paths)
 
         journey_paths = consolidate_paths(all_paths)
-        expected_journey = [
-            ['Oa, Ob', 'Da'],
-        ]
+        expected_journey = [[('Oa, Ob', 0), ('Da', 1)]]
         self.assertTrue(journey_paths == expected_journey)
 
     def test_journey_simple_graph_no_jumps(self):
@@ -139,8 +138,8 @@ class JourneyTestCase(TestCase):
         destinations = list(Destination.objects.filter(connectivity_statement=cs))
 
         expected_paths = [
-            ['Ob', 'V1a', 'Da'],
-            ['Oa', 'V1a', 'Da'],
+            [('Ob', 0), ('V1a', 1), ('Da', 2)],
+            [('Oa', 0), ('V1a', 1), ('Da', 2)]
         ]
 
         all_paths = generate_paths(origins, vias, destinations)
@@ -149,9 +148,7 @@ class JourneyTestCase(TestCase):
         expected_paths.sort()
         self.assertTrue(all_paths == expected_paths)
 
-        expected_journey = [
-            ['Oa, Ob', 'V1a', 'Da'],
-        ]
+        expected_journey = [[('Oa, Ob', 0), ('V1a', 1), ('Da', 2)]]
         journey_paths = consolidate_paths(all_paths)
         self.assertTrue(journey_paths == expected_journey)
 
@@ -195,17 +192,96 @@ class JourneyTestCase(TestCase):
         all_paths = generate_paths(origins, vias, destinations)
 
         expected_paths = [
-            ['Oa', 'V1a', 'Da'],
-            ['Oa', 'V1b', 'Da'],
-            ['Ob', 'V1a', 'Da'],
-            ['Ob', 'V1b', 'Da']]
+            [('Oa', 0), ('V1a', 1), ('Da', 2)],
+            [('Oa', 0), ('V1b', 1), ('Da', 2)],
+            [('Ob', 0), ('V1a', 1), ('Da', 2)],
+            [('Ob', 0), ('V1b', 1), ('Da', 2)]
+        ]
 
         all_paths.sort()
         expected_paths.sort()
         self.assertTrue(all_paths == expected_paths)
 
         expected_journey = [
-            ['Oa, Ob', 'V1a, V1b', 'Da'],
+            [('Oa, Ob', 0), ('V1a, V1b', 1), ('Da', 2)]
         ]
+        journey_paths = consolidate_paths(all_paths)
+        self.assertTrue(journey_paths == expected_journey)
+
+    def test_journey_complex_graph(self):
+        #####################################################################
+
+        # Oa -> V3a -> Da
+        # Oa -> V1a -> V2a -> V4a -> Da
+        # Oa -> V1a -> V2a -> V3a -> Da
+        # Oa -> V1a -> V2b -> Da
+        # Ob -> V1a -> V2a -> V4a -> Da
+        # Ob -> V1a -> V2a -> V3a -> Da
+        # Ob -> V1a -> V2b -> Da
+
+        sentence = Sentence.objects.create()
+        cs = ConnectivityStatement.objects.create(sentence=sentence)
+
+        # Create Anatomical Entities
+        origin_a = AnatomicalEntity.objects.create(name='Oa')
+        origin_b = AnatomicalEntity.objects.create(name='Ob')
+        via1_a = AnatomicalEntity.objects.create(name='V1a')
+        via2_a = AnatomicalEntity.objects.create(name='V2a')
+        via2_b = AnatomicalEntity.objects.create(name='V2b')
+        via3_a = AnatomicalEntity.objects.create(name='V3a')
+        via4_a = AnatomicalEntity.objects.create(name='V4a')
+        destination_a = AnatomicalEntity.objects.create(name='Da')
+
+        # Add origins
+        cs.origins.add(origin_a, origin_b)
+
+        # Create Vias
+        via1 = Via.objects.create(connectivity_statement=cs, order=0)
+        via1.anatomical_entities.add(via1_a)
+        via1.from_entities.add(origin_a, origin_b)
+
+        via2 = Via.objects.create(connectivity_statement=cs, order=1)
+        via2.anatomical_entities.add(via2_a, via2_b)
+        via2.from_entities.add(via1_a)
+
+        via3 = Via.objects.create(connectivity_statement=cs, order=2)
+        via3.anatomical_entities.add(via3_a)
+        via3.from_entities.add(via2_a, origin_a)
+
+        via4 = Via.objects.create(connectivity_statement=cs, order=3)
+        via4.anatomical_entities.add(via4_a)
+        via4.from_entities.add(via2_a)
+
+        # Create Destination
+        destination = Destination.objects.create(connectivity_statement=cs)
+        destination.anatomical_entities.add(destination_a)
+        destination.from_entities.add(via4_a, via3_a, via2_b)
+
+        ######################################################################
+
+        origins = list(cs.origins.all())
+        vias = list(Via.objects.filter(connectivity_statement=cs))
+        destinations = list(Destination.objects.filter(connectivity_statement=cs))
+
+        all_paths = generate_paths(origins, vias, destinations)
+
+        expected_paths = [
+            [('Oa', 0), ('V3a', 3), ('Da', 5)],
+            [('Oa', 0), ('V1a', 1), ('V2a', 2), ('V4a', 4), ('Da', 5)],
+            [('Oa', 0), ('V1a', 1), ('V2a', 2), ('V3a', 3), ('Da', 5)],
+            [('Oa', 0), ('V1a', 1), ('V2b', 2), ('Da', 5)],
+            [('Ob', 0), ('V1a', 1), ('V2a', 2), ('V4a', 4), ('Da', 5)],
+            [('Ob', 0), ('V1a', 1), ('V2a', 2), ('V3a', 3), ('Da', 5)],
+            [('Ob', 0), ('V1a', 1), ('V2b', 2), ('Da', 5)]
+        ]
+
+        all_paths.sort()
+        expected_paths.sort()
+        self.assertTrue(all_paths == expected_paths)
+
+        expected_journey = [[('Oa, Ob', 0), ('V1a', 1), ('V2a', 2), ('V3a', 3), ('Da', 5)],
+                            [('Oa, Ob', 0), ('V1a', 1), ('V2a', 2), ('V4a', 4), ('Da', 5)],
+                            [('Oa, Ob', 0), ('V1a', 1), ('V2b', 2), ('Da', 5)],
+                            [('Oa', 0), ('V3a', 3), ('Da', 5)]]
         journey_paths = consolidate_paths(all_paths)
         self.assertTrue(journey_paths == expected_journey)
