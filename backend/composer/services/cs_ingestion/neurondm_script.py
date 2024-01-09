36eb36edd806e16361a6babd1a248fed05f06aed
afonso <afonsobspinto@gmail.com>
2024-01-09 13:00:33 +0000

    SCKAN-226 feat: Merge destinations

--- backend/composer/services/cs_ingestion/neurondm_script.py
+++ backend/composer/services/cs_ingestion/neurondm_script.py
@@ -1,32 +1,31 @@
 import csv
-import logging
 import os
-from typing import Set, Dict
+from typing import Set
 
 import rdflib
-from pyontutils.core import OntGraph, OntResIri, OntResPath
-from pyontutils.namespaces import rdfs, ilxtr
+from neurondm import orders
 from neurondm.core import Config, graphBase, log
 from neurondm.core import OntTerm, OntId, RDFL
-from neurondm import orders
+from pyontutils.core import OntGraph, OntResIri, OntResPath
+from pyontutils.namespaces import rdfs, ilxtr
 
 
 class Origin:
-    def __init__(self, anatomical_entity_uri: str):
-        self.anatomical_entity = anatomical_entity_uri
+    def __init__(self, anatomical_entities_uri: Set):
+        self.anatomical_entities = anatomical_entities_uri
 
 
 class Via:
-    def __init__(self, anatomical_entity_uri: str, from_entities: Set, order: int, type: str):
-        self.anatomical_entity = anatomical_entity_uri
+    def __init__(self, anatomical_entities_uri: Set, from_entities: Set, order: int, type: str):
+        self.anatomical_entities = anatomical_entities_uri
         self.from_entities = from_entities
         self.order = order
         self.type = type
 
 
 class Destination:
-    def __init__(self, anatomical_entity_uri: str, from_entities: Set, type: str):
-        self.anatomical_entity = anatomical_entity_uri
+    def __init__(self, anatomical_entities_uri: Set, from_entities: Set, type: str):
+        self.anatomical_entities = anatomical_entities_uri
         self.from_entities = from_entities
         self.type = type
 
@@ -91,14 +90,16 @@ def get_connections(n, lpes):
                                                         ilxtr.hasAxonSensorySubcellularElementIn: 'AFFERENT-T'})
     expected_vias = create_uri_type_dict(lpes, {ilxtr.hasAxonLocatedIn: 'AXON', ilxtr.hasDendriteLocatedIn: 'DENDRITE'})
 
-    origins, vias_temp, destinations = process_connections(partial_order,
-                                                           set(expected_origins),
-                                                           expected_vias,
-                                                           expected_destinations
-                                                           )
+    tmp_origins, tmp_vias, tmp_destinations = process_connections(partial_order,
+                                                                  set(expected_origins),
+                                                                  expected_vias,
+                                                                  expected_destinations
+                                                                  )
 
-    vias = merge_vias(vias_temp)
-    return origins, vias, destinations
+    # origins = merge_origins(tmp_origins)
+    vias = merge_vias(tmp_vias)
+    destinations = merge_destinations(tmp_destinations)
+    return tmp_origins, vias, destinations
 
 
 def create_uri_type_dict(lpes_func, predicate_type_map):
@@ -117,8 +118,8 @@ def process_connections(path, expected_origins, expected_vias, expected_destinat
     if isinstance(path, tuple):
         if path[0] == rdflib.term.Literal('blank'):
             for remaining_path in path[1:]:
-                process_connections(remaining_path, expected_origins, expected_vias, expected_destinations,
-                                    from_entities=set(), depth=0, result=result)
+                process_connections(remaining_path, expected_origins, expected_vias, expected_destinations, set(),
+                                    depth=0, result=result)
         else:
             current_entity_uri = process_entity(path[0]).toPython()
 
@@ -127,14 +128,15 @@ def process_connections(path, expected_origins, expected_vias, expected_destinat
 
             # Determine the type of the current entity based on the expected lists
             if current_entity_uri in expected_origins:
-                result['origins'].append(Origin(current_entity_uri))
+                result['origins'].append(Origin({current_entity_uri}))
                 depth = 0  # Reset depth after each origin
             elif current_entity_uri in expected_vias:
-                result['vias'].append(Via(current_entity_uri, from_entities, depth, expected_vias[current_entity_uri]))
-                depth += 1  # Increment depth only for vias
+                result['vias'].append(
+                    Via({current_entity_uri}, from_entities, depth, expected_vias[current_entity_uri]))
+                depth += 1
             elif current_entity_uri in expected_destinations:
                 result['destinations'].append(
-                    Destination(current_entity_uri, from_entities, expected_destinations[current_entity_uri]))
+                    Destination({current_entity_uri}, from_entities, expected_destinations[current_entity_uri]))
 
             # Process the next level structures, carrying over from_entities as a set
             for next_structure in path[1:]:
@@ -154,14 +156,41 @@ def process_entity(entity):
 
 
 def merge_vias(vias):
+    vias = merge_vias_from_entities(vias)
+    return merge_vias_anatomical_entities(vias)
+
+
+def merge_vias_from_entities(vias):
     merged_vias = {}
     for via in vias:
-        key = (via.anatomical_entity, via.order, via.type)
+        key = (frozenset(via.anatomical_entities), via.order, via.type)
         if key not in merged_vias:
-            merged_vias[key] = Via(via.anatomical_entity, set(), via.order, via.type)
+            merged_vias[key] = Via(via.anatomical_entities, set(), via.order, via.type)
         merged_vias[key].from_entities.update(via.from_entities)
 
     return list(merged_vias.values())
+
+
+def merge_vias_anatomical_entities(vias):
+    merged_vias = {}
+    for via in vias:
+        key = (via.order, via.type, frozenset(via.from_entities))
+        if key not in merged_vias:
+            merged_vias[key] = Via(set(), via.from_entities, via.order, via.type)
+        merged_vias[key].anatomical_entities.update(via.anatomical_entities)
+
+    return list(merged_vias.values())
+
+
+def merge_destinations(destinations):
+    merged_destinations = {}
+    for destination in destinations:
+        key = (frozenset(destination.anatomical_entities), destination.type)
+        if key not in merged_destinations:
+            merged_destinations[key] = Destination(destination.anatomical_entities, set(), destination.type)
+        merged_destinations[key].from_entities.update(destination.from_entities)
+
+    return list(merged_destinations.values())
 
 
 ## Based on:
