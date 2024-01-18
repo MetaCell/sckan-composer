acbd54b3529dff3b2d58bd2f1cb769302720a656
afonso <afonsobspinto@gmail.com>
2024-01-18 10:53:37 +0000

    SCKAN-226 feat: Use .update and .create instead of .update_or_create

--- backend/composer/services/cs_ingestion/cs_ingestion_services.py
+++ backend/composer/services/cs_ingestion/cs_ingestion_services.py
@@ -180,35 +180,31 @@ def get_or_create_sentence(statement: Dict) -> Tuple[Sentence, bool]:
 
 def create_or_update_connectivity_statement(statement: Dict, sentence: Sentence) -> Tuple[ConnectivityStatement, bool]:
     reference_uri = statement[ID]
+    defaults = {
+        "sentence": sentence,
+        "knowledge_statement": statement[LABEL],
+        "sex": get_sex(statement),
+        "circuit_type": get_circuit_type(statement),
+        "functional_circuit_role": get_functional_circuit_role(statement),
+        "phenotype": get_phenotype(statement),
+        "projection_phenotype": get_projection_phenotype(statement),
+        "reference_uri": statement[ID],
+        "state": CSState.EXPORTED,
+    }
 
-    connectivity_statement, created = ConnectivityStatement.objects.update_or_create(
-        reference_uri__exact=reference_uri,
-        defaults={
-            "sentence": sentence,
-            "knowledge_statement": statement[LABEL],
-            "sex": get_sex(statement),
-            "circuit_type": get_circuit_type(statement),
-            "functional_circuit_role": get_functional_circuit_role(statement),
-            "phenotype": get_phenotype(statement),
-            "projection_phenotype": get_projection_phenotype(statement),
-            "reference_uri": statement[ID],
-        }
-    )
+    if ConnectivityStatement.objects.filter(reference_uri__exact=reference_uri).exists():
+        ConnectivityStatement.objects.filter(reference_uri__exact=reference_uri).update(**defaults)
+        connectivity_statement = ConnectivityStatement.objects.get(reference_uri__exact=reference_uri)
+        created = False
+    else:
+        connectivity_statement = ConnectivityStatement.objects.create(**defaults)
+        created = True
 
     update_many_to_many_fields(connectivity_statement, statement)
-    if created:
-        do_transition_to_exported(connectivity_statement)
-    else:
+    if not created:
         add_ingestion_system_note(connectivity_statement)
 
     return connectivity_statement, created
-
-
-def do_transition_to_exported(connectivity_statement):
-    # Change state from draft (default) to exported
-    system_user = User.objects.get(username="system")
-    connectivity_statement.exported_from_ingestion(by=system_user)
-    connectivity_statement.save()
 
 
 def get_sex(statement: Dict) -> Sex:
