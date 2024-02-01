37ea787ec1d408d5cabc30e44f12fc9a46ccc436
D. Gopal Krishna <nikhil0223@gmail.com>
2024-02-01 15:06:12 +0530

    #256 permissions update - cut it short

--- backend/composer/api/permissions.py
+++ backend/composer/api/permissions.py
@@ -4,10 +4,6 @@ from composer.enums import CSState
 # Permission Checks: Only staff users can update a Connectivity Statement when it is in state exported
 class IsStaffUserIfExportedStateInConnectivityStatement(permissions.BasePermission):
 	def has_object_permission(self, request, view, obj):
-		if request.method in permissions.SAFE_METHODS:
-			return True
-		else:
-			if obj.state == CSState.EXPORTED:
-				return request.user.is_staff
-			else:
-				return True
+		if (request.method not in permissions.SAFE_METHODS) and (obj.state == CSState.EXPORTED):
+			return request.user.is_staff
+		return True
