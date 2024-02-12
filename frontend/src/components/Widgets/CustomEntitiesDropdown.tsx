184875e64d348cdd9e37685fb937f93fef09d897
ddelpiano <dario@metacell.us>
2024-02-12 11:04:16 +0100

    SCKAN-262: fixing linter issues

--- frontend/src/components/Widgets/CustomEntitiesDropdown.tsx
+++ frontend/src/components/Widgets/CustomEntitiesDropdown.tsx
@@ -18,7 +18,6 @@ import {
 } from "@mui/material";
 import {CheckedItemIcon, CheckedItemIconBG, UncheckedItemIcon} from "../icons";
 import HoveredOptionContent from "./HoveredOptionContent";
-import ClearOutlinedIcon from "@mui/icons-material/ClearOutlined";
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 import ArrowDropUpIcon from "@mui/icons-material/ArrowDropUp";
 import theme from "../../theme/Theme";
