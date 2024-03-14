4a0e43f9f67c8753fc828ef91ac66453a2f181aa
afonso <afonsobspinto@gmail.com>
2024-03-14 12:52:52 +0000

    SCKAN-274 feat: Update ingest anatomical entities for the new file

--- backend/composer/management/commands/ingest_anatomical_entities.py
+++ backend/composer/management/commands/ingest_anatomical_entities.py
@@ -19,19 +19,23 @@ class Command(BaseCommand):
         parser.add_argument("--show_complete_logs", action='store_true',
                             help="Show detailed logs during processing")
 
-    def _process_anatomical_entity(self, name, ontology_uri, synonym, update_names, show_complete_logs):
+    def _process_anatomical_entity(self, name, ontology_uri, synonym, update_names, show_complete_logs, processed_uris):
         try:
+            is_first_occurrence = ontology_uri not in processed_uris
+
             anatomical_entity, created = AnatomicalEntity.objects.get_or_create(
                 ontology_uri=ontology_uri,
                 defaults={"name": name},
             )
-            if not created and update_names:
+            if not created and update_names and is_first_occurrence:
                 if anatomical_entity.name != name:
                     anatomical_entity.name = name
                     anatomical_entity.save()
                     if show_complete_logs:
                         self.stdout.write(
                             self.style.SUCCESS(f"Updated {anatomical_entity.ontology_uri} name to {name}."))
+
+            processed_uris.add(ontology_uri)
 
             if synonym:
                 synonym_exists = anatomical_entity.synonyms.filter(alias__iexact=synonym).exists()
@@ -49,12 +53,13 @@ class Command(BaseCommand):
         show_complete_logs = options['show_complete_logs']
 
         for csv_file in options["csv_files"]:
+            processed_uris = set()
             try:
                 with open(csv_file, "r", encoding="utf-8", errors="ignore") as f:
                     total_lines = sum(1 for _ in f)
 
                 with open(csv_file, newline="", encoding="utf-8", errors="ignore") as csvfile:
-                    reader = csv.DictReader(csvfile, delimiter=";", quotechar='"')
+                    reader = csv.DictReader(csvfile, delimiter=",", quotechar='"')
                     current_line = 0
                     for row in reader:
                         current_line += 1
@@ -65,7 +70,8 @@ class Command(BaseCommand):
                         name = row[NAME].strip()
                         synonym = row[SYNONYM].strip() if row[SYNONYM] else None
 
-                        self._process_anatomical_entity(name, ontology_uri, synonym, update_names, show_complete_logs)
+                        self._process_anatomical_entity(name, ontology_uri, synonym, update_names, show_complete_logs,
+                                                        processed_uris)
             except FileNotFoundError:
                 self.stdout.write(self.style.ERROR(f"File {csv_file} does not exist."))
             except Exception as e:
