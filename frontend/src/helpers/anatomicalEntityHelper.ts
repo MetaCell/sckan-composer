bea1ade9c385015723b56e218a62f7bd5ffc6c4e
D. Gopal Krishna <nikhil0223@gmail.com>
2024-04-02 11:05:35 +0530

    #276 show region layer uri and synonyms

--- frontend/src/helpers/anatomicalEntityHelper.ts
+++ frontend/src/helpers/anatomicalEntityHelper.ts
@@ -1,8 +1,58 @@
 import {AnatomicalEntity} from "../apiclient/backend";
+import { OptionDetail } from "../types";
+
+
+export const getAnatomicalEntityDetails = (entity: AnatomicalEntity): OptionDetail[] => {
+  let details: OptionDetail[] = [
+    {
+      title: "Label",
+      value: getName(entity),
+    },
+    {
+      title: "Synonyms",
+      value: entity.synonyms
+    }
+  ]
+  if (entity.region_layer) {
+    if (entity.region_layer.region) {
+      details.push(
+        {
+          title: "Region",
+          value: getRegionName(entity)
+        },
+        {
+          title: "Region URI",
+          value: getRegionURI(entity)
+        }
+      )
+    }
+    if (entity.region_layer.layer) {
+      details.push(
+        {
+          title: "Layer",
+          value: getLayerName(entity)
+        },
+        {
+          title: "Layer URI",
+          value: getLayerURI(entity)
+        }
+      )
+    }
+  }
+  if (entity.simple_entity) {
+    details.push(
+      {
+        title: "Ontology URI",
+        value: getURI(entity)
+      }
+    )
+  }
+  return details
+}
 
 export const getName = (anatomicalEntity: AnatomicalEntity) => {
   if (anatomicalEntity.region_layer) {
-    return `${anatomicalEntity.region_layer.region.name},${anatomicalEntity.region_layer.layer.name}`
+    return `${anatomicalEntity.region_layer.region.name} - ${anatomicalEntity.region_layer.layer.name}`
   }
   return anatomicalEntity.simple_entity.name
 }
@@ -13,3 +63,34 @@ export const getURI = (anatomicalEntity: AnatomicalEntity) => {
   }
   return anatomicalEntity.simple_entity.ontology_uri
 }
+
+export const getLayerName = (anatomicalEntity: AnatomicalEntity) => {
+  if (anatomicalEntity.region_layer) {
+    return anatomicalEntity.region_layer.layer.name
+  }
+  return "N/A"
+}
+
+export const getRegionName = (anatomicalEntity: AnatomicalEntity) => {
+  if (anatomicalEntity.region_layer) {
+    return anatomicalEntity.region_layer.region.name
+  }
+  return "N/A"
+}
+
+export const getLayerURI = (anatomicalEntity: AnatomicalEntity) => {
+  if (anatomicalEntity.region_layer) {
+    return anatomicalEntity.region_layer.layer.ontology_uri
+  }
+  return "N/A"
+}
+
+export const getRegionURI = (anatomicalEntity: AnatomicalEntity) => {
+  if (anatomicalEntity.region_layer) {
+    return anatomicalEntity.region_layer.region.ontology_uri
+  }
+  return "N/A"
+}
+
+
+
