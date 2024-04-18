d5784716779bcd6c0caf351f960d66bebf583a38
D. Gopal Krishna <nikhil0223@gmail.com>
2024-04-18 15:09:41 +0530

    ESCKAN-35 renaming and cleanup

--- backend/composer/api/handlers.py
+++ backend/composer/api/handlers.py
@@ -1,10 +1,9 @@
 from corsheaders.signals import check_request_enabled
 
 
-def cors_allow_api_to_origins(sender, request, **kwargs):
-    """
-    Allow API requests to be made from the specified origins.
-	"""
+def cors_allow_knowledge_statement_request(sender, request, **kwargs):
+    """Allow API requests to be made for the knowledge statement."""
     return request.path == '/api/composer/knowledge-statement/'
 
-check_request_enabled.connect(cors_allow_api_to_origins)
+
+check_request_enabled.connect(cors_allow_knowledge_statement_request)
