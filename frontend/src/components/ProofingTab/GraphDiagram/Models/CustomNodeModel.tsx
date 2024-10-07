7b8adcc3e145bec2161b04529e363c5dcb35c8c5
afonso <afonsobspinto@gmail.com>
2024-10-07 10:35:38 +0100

    CSCKAN-300 feat: Fix serialization

--- frontend/src/components/ProofingTab/GraphDiagram/Models/CustomNodeModel.tsx
+++ frontend/src/components/ProofingTab/GraphDiagram/Models/CustomNodeModel.tsx
@@ -32,4 +32,32 @@ export class CustomNodeModel extends NodeModel {
     getOptions(): CustomNodeOptions {
         return super.getOptions() as CustomNodeOptions;
     }
+
+    // Override serialize to include custom properties and options
+    serialize() {
+        return {
+            ...super.serialize(),
+            customType: this.customType,
+            name: this.name,
+            externalId: this.externalId,
+            forward_connection: this.getOptions().forward_connection,
+            from: this.getOptions().from,
+            to: this.getOptions().to,
+            anatomicalType: this.getOptions().anatomicalType,
+        };
+    }
+
+    // Override deserialize to set custom properties and options
+    deserialize(event: any): void {
+        super.deserialize(event);
+        this.customType = event.data.customType;
+        this.name = event.data.name;
+        this.externalId = event.data.externalId;
+
+        // Set custom options
+        this.getOptions().forward_connection = event.data.forward_connection || [];
+        this.getOptions().from = event.data.from || [];
+        this.getOptions().to = event.data.to || [];
+        this.getOptions().anatomicalType = event.data.anatomicalType || '';
+    }
 }
