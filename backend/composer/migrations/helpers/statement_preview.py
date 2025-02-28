30353a28001a2837321d68ed797be7e7be446d6c
ddelpiano <dario@metacell.us>
2025-02-28 19:11:53 +0100

    fixing migration

--- backend/composer/migrations/helpers/statement_preview.py
+++ backend/composer/migrations/helpers/statement_preview.py
@@ -1,3 +1,4 @@
+from composer.enums import Laterality
 from composer.utils import join_entities
 from django.apps import apps
 
@@ -20,23 +21,19 @@ def get_migration_prefix_for_statement_preview(cs) -> str:
 
 
 def get_migration_suffix_for_statement_preview(cs):
-    ConnectivityStatement = apps.get_model(
-        'composer', 'ConnectivityStatement')
-    connectivity_statement = ConnectivityStatement.objects.get(
-        id=cs.id)
 
-    circuit_type = connectivity_statement.get_circuit_type_display(
-    ) if connectivity_statement.circuit_type else None
-    projection = connectivity_statement.get_projection_display(
-    ) if connectivity_statement.projection else None
+    circuit_type = cs.get_circuit_type_display(
+    ) if cs.circuit_type else None
+    projection = cs.get_projection_display(
+    ) if cs.projection else None
     projection_phenotype = str(
-        connectivity_statement.projection_phenotype) if connectivity_statement.projection_phenotype else ''
+        cs.projection_phenotype) if cs.projection_phenotype else ''
 
-    laterality_description = connectivity_statement.get_laterality_description()
-    apinatomy = connectivity_statement.apinatomy_model if connectivity_statement.apinatomy_model else ""
+    laterality_description = get_laterality_description(cs)
+    apinatomy = cs.apinatomy_model if cs.apinatomy_model else ""
 
     origin_names = [
-        origin.name for origin in connectivity_statement.origins.all()]
+        get_name(origin) for origin in cs.origins.all()]
     origins = join_entities(origin_names)
 
     if not origins:
@@ -58,3 +55,18 @@ def get_migration_suffix_for_statement_preview(cs):
     if apinatomy:
         statement += f" It is described in {apinatomy} model."
     return statement
+
+
+def get_name(instance):
+    if instance.simple_entity:
+        return str(instance.simple_entity)
+    if instance.region_layer:
+        return str(instance.region_layer)
+    return 'Unknown Anatomical Entity'
+
+def get_laterality_description(instance):
+    laterality_map = {
+        Laterality.RIGHT.value: "on the right side of the body",
+        Laterality.LEFT.value: "on the left side of the body",
+    }
+    return laterality_map.get(instance.laterality, None)
