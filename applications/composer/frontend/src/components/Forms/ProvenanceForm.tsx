aceeba7208787ccef8024135aa70a3d36dd3bae8
afonso pinto <afonsobspinto@gmail.com>
2025-09-24 17:32:20 +0100

    SCKAN-434 feat: Add isLoading external prop to FormBase

--- applications/composer/frontend/src/components/Forms/ProvenanceForm.tsx
+++ applications/composer/frontend/src/components/Forms/ProvenanceForm.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import { FormBase } from './FormBase'
 import { jsonSchemas } from '../../services/JsonSchema'
 import provenanceService from '../../services/ProvenanceService'
@@ -9,6 +9,7 @@ import {ChangeRequestStatus} from "../../helpers/settings";
 
 const ProvenancesForm = (props: any) => {
   const { provenancesData, setter, extraData, isDisabled } = props
+  const [isLoading, setIsLoading] = useState(false)
 
   const { schema, uiSchema } = jsonSchemas.getProvenanceSchema()
   const copiedSchema = JSON.parse(JSON.stringify(schema));
@@ -36,6 +37,7 @@ const ProvenancesForm = (props: any) => {
       return;
     }
     
+    setIsLoading(true)
     return checkOwnership(
       extraData.connectivity_statement_id,
       async () => {
@@ -48,9 +50,12 @@ const ProvenancesForm = (props: any) => {
           } else {
             alert('Failed to save provenance. Please try again.');
           }
+        }).finally(() => {
+          setIsLoading(false)
         })
       },
       () => {
+        setIsLoading(false)
         return ChangeRequestStatus.CANCELLED;
         },
       getOwnershipAlertMessage // message to show when ownership needs to be reassigned
@@ -112,13 +117,16 @@ const ProvenancesForm = (props: any) => {
       data: provenancesData?.map((row: Provenance) => ({id: row.id, label: row.uri, enableClick: isValidUrl(row.uri) })) || [],
       placeholder: isDisabled ? null : 'Enter Provenances (Press Enter to add a Provenance)',
       removeChip: function(provenanceId: any) {
+        setIsLoading(true)
         return checkOwnership(
           extraData.connectivity_statement_id,
           async () => {
            await provenanceService.delete(provenanceId, extraData.connectivity_statement_id)
             refresh()
+            setIsLoading(false)
           },
           () => {
+            setIsLoading(false)
             return ChangeRequestStatus.CANCELLED;
             },
           getOwnershipAlertMessage // message to show when ownership needs to be reassigned
@@ -150,6 +158,7 @@ const ProvenancesForm = (props: any) => {
         extraData={extraData}
         setter={() => refresh()}
         disabled={isDisabled}
+        isLoading={isLoading}
       />
   )
 }
